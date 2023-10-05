fd4260e279c5896723c2f74b0f30087880439302
glorfindel31 <116696371+Glorfindel31@users.noreply.github.com>
2023-10-05 14:32:44 +0700

    favicon addition

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import './globals.css';
 import 'tailwindcss/tailwind.css';
 import {Inter} from 'next/font/google';
 import {Providers} from './providers';
+import Head from 'next/head';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -13,6 +14,9 @@ export const metadata = {
 export default function RootLayout({children}) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
