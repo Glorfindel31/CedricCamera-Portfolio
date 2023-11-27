f43606ec8786b45fc7f6c73d517e55feeaa3f433
glorfindel31 <116696371+Glorfindel31@users.noreply.github.com>
2023-11-27 17:54:49 +0700

    Initial Publish

---
+++ app/prints/details.jsx
@@ -0,0 +1,136 @@
+export const imagesDetails = [
+  {
+    public_id: 'digital/others/rice-patterns',
+    caption: 'An aerial view of a field of dead rice stalks in the middle of the day.',
+  },
+  {
+    public_id: 'digital/others/into-the-myst',
+    caption: 'A cloudy sky over a mountain range in Vietnam.',
+  },
+  {
+    public_id: 'digital/others/curiosity',
+    caption:
+      'Two brown cows standing next to each other in a field. One is looking at the camera.',
+  },
+  {
+    public_id: 'digital/others/framed-sky',
+    caption:
+      'A building framed by a bridge and an other building catchs the light and the clouds reflection.',
+  },
+  {
+    public_id: 'digital/others/serious-matters',
+    caption: 'An old man in Yen Tinh, Vietnam, looks at the camera. Why so serious?',
+  },
+  {
+    public_id: 'digital/others/ready-to-fly',
+    caption: "Vanishing point of Bangkok's Suvarnabhumi Airport's runway.",
+  },
+  {
+    public_id: 'digital/others/open-sit',
+    caption: 'Hoi An by night, small open sit waiting for customers.',
+  },
+  {
+    public_id: 'digital/others/lights-on-ideas',
+    caption: 'Hoi An, in a coffeshop an empty desk, but not for long.',
+  },
+  {
+    public_id: 'digital/others/small-little-trees',
+    caption:
+      'Vietnam landscapes are breathtaking, those small threes on the edge of the mountain like the feathers on the head of the peacock.',
+  },
+  {
+    public_id: 'digital/others/heads-in-the-clouds',
+    caption: 'The sun, burning the clouds.',
+  },
+  {
+    public_id: 'digital/others/transparency-leaf-one',
+    caption: 'The first of 3, a leaf on a white background. It is all about details.',
+  },
+  {
+    public_id: 'digital/others/line-breaking',
+    caption: 'Urban, the petrole by night.',
+  },
+  {
+    public_id: 'digital/others/totally-fine',
+    caption: 'Wild car caught drinking petroleum in the middle of the night.',
+  },
+  {
+    public_id: 'digital/others/transparency-leaf-three',
+    caption: 'The third of 3, a leaf on a white background. It is all about details.',
+  },
+  {
+    public_id: 'digital/others/transparency-leaf-two',
+    caption: 'The second of 3, a leaf on a white background. It is all about details.',
+  },
+  {
+    public_id: 'digital/others/ominous-build',
+    caption: 'Dark in the night the building is looking in your direction, ominous.',
+  },
+  {
+    public_id: 'digital/others/graal',
+    caption: 'Vietnamese graal, the cup of the lord.',
+  },
+  {
+    public_id: 'film/others/blue-in-concret',
+    caption: 'A building framed the beauty of the sky.',
+  },
+  {
+    public_id: 'film/others/eyes-away',
+    caption:
+      'Far in the northen mountains of Vietnam, I found myself trying to catch this family without being seen, I failed.',
+  },
+  {
+    public_id: 'film/others/glass-drops',
+    caption: 'Finding the beauty out of gloominess.',
+  },
+  {
+    public_id: 'film/others/dead-end',
+    caption: 'Bangkok, a dead end at the airport.',
+  },
+  {
+    public_id: 'film/others/bended-arround',
+    caption:
+      'My pation for patterns never ends, in those metal bars ready to be recycled.',
+  },
+  {
+    public_id: 'film/others/petrolium-three',
+    caption: 'Outside in the night, the petrole is burning.',
+  },
+  {
+    public_id: 'film/others/warm-sun',
+    caption: 'Burning sky, ready for the night.',
+  },
+  {
+    public_id: 'film/others/time-to-bath',
+    caption: 'Youngling chilling in the water, after a long hot day of summer.',
+  },
+  {
+    public_id: 'film/others/hanoi-house-two',
+    caption:
+      'Traditional house in Hanoi, Vietnam. This house must have been photographed a million times.',
+  },
+  {
+    public_id: 'film/others/half-in-the-dark',
+    caption: 'Two chairs and one table emerging from the dark. lucky them.',
+  },
+  {
+    public_id: 'film/others/reflection',
+    caption: "Bangkok's buildings are like millions of mirrors.",
+  },
+  {
+    public_id: 'film/others/hanoi-house',
+    caption: 'Some architecture devilry in Hanoi, Vietnam.',
+  },
+  {
+    public_id: 'film/others/paper-leaves',
+    caption: 'Paper leaves, a new way to see the world.',
+  },
+  {
+    public_id: 'film/others/lonely-trolley',
+    caption: 'Some trolley got tired and left behind in the middle of the night.',
+  },
+  {
+    public_id: 'film/others/lonely-sleeper',
+    caption: "Don't forget to give yourself a break.",
+  },
+];
