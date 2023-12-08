f0ed2e612c74693fb6f106b64a9d0d6ebae2c94d
glorfindel31 <cedric31flo@gmail.com>
2023-12-08 16:10:05 +0700

    Update ImageGallery component styling

--- components/ImageGallery.jsx
+++ components/ImageGallery.jsx
@@ -62,9 +62,9 @@ const ImageGallery = ({public_id, asset_id, folder}) => {
           {isModalLoaded && (
             <div
               role="status"
-              className="absolute space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center w-full h-full"
+              className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center w-[60vw] h-[90vh]"
             >
-              <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+              <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
                 <svg
                   className="w-10 h-10 text-gray-200 dark:text-gray-600"
                   aria-hidden="true"
