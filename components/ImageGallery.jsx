d05a4c53fa0c3d45ca1378251a2d7f02b8f55c08
glorfindel31 <116696371+Glorfindel31@users.noreply.github.com>
2023-12-08 17:52:34 +0700

    Update ImageGallery and Modal styles

--- components/ImageGallery.jsx
+++ components/ImageGallery.jsx
@@ -21,52 +21,15 @@ const ImageGallery = ({public_id, asset_id, folder}) => {
   return (
     <>
       <div className={style.item} onClick={modalHandler}>
-        {isLoaded && (
-          <div
-            role="status"
-            className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center"
-          >
-            <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
-              <svg
-                className="w-10 h-10 text-gray-200 dark:text-gray-600"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 18"
-              >
-                <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-              </svg>
-            </div>
-            <span className="sr-only">Loading...</span>
-          </div>
-        )}
-        <Image
-          className={`transition-opacity ${isLoaded ? 'opacity-0' : ''} duration-1000`}
-          onLoad={event => {
-            setIsLoaded(false);
-            event.target.classList.remove('opacity-0');
-          }}
-          src={`https://res.cloudinary.com/dduwp6ob6/image/upload/f_auto,q_auto/${public_id}`}
-          quality="auto"
-          width={300}
-          height={400}
-          sizes="(max-width: 300px) 50vw, 20vw"
-          alt={`Cedric Florentin - Copyrighted Image from my portfolio named ${
-            (public_id, asset_id)
-          }`}
-          loading="lazy"
-        />
-      </div>
-      {openModal && (
-        <Modal isOpen={openModal} closeModal={modalHandler}>
-          {isModalLoaded && (
+        <div className="relative">
+          {isLoaded && (
             <div
               role="status"
-              className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center w-[60vw] h-[90vh]"
+              className="absolute inset-0 flex items-center justify-center space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center"
             >
-              <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+              <div className="flex items-center justify-center w-full h-full bg-gray-400 sm:w-96 dark:bg-gray-800">
                 <svg
-                  className="w-10 h-10 text-gray-200 dark:text-gray-600"
+                  className="w-10 h-10 text-gray-100 dark:text-gray-600"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="currentColor"
@@ -75,7 +38,44 @@ const ImageGallery = ({public_id, asset_id, folder}) => {
                   <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
                 </svg>
               </div>
-              <span className="sr-only">Loading...</span>
+            </div>
+          )}
+          <Image
+            className={`transition-opacity ${isLoaded ? 'opacity-0' : ''} duration-1000`}
+            onLoad={event => {
+              setIsLoaded(false);
+              event.target.classList.remove('opacity-0');
+            }}
+            src={`https://res.cloudinary.com/dduwp6ob6/image/upload/f_auto,q_auto/${public_id}`}
+            quality="auto"
+            width={300}
+            height={300}
+            sizes="(max-width: 300px) 50vw, 20vw"
+            alt={`Cedric Florentin - Copyrighted Image from my portfolio named ${
+              (public_id, asset_id)
+            }`}
+            loading="lazy"
+          />
+        </div>
+      </div>
+      {openModal && (
+        <Modal isOpen={openModal} closeModal={modalHandler}>
+          {isModalLoaded && (
+            <div
+              role="status"
+              className="absolute inset-0 flex items-center justify-center space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center"
+            >
+              <div className="flex items-center justify-center w-full h-full bg-gray-400 sm:w-96 dark:bg-gray-800">
+                <svg
+                  className="w-10 h-10 text-gray-100 dark:text-gray-600"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 18"
+                >
+                  <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                </svg>
+              </div>
             </div>
           )}
           <Image
